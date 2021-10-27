3dd487f0ee61bc95cb77bc2ddee092444e2276de
Hanwool Lee <hanwool26@gmail.com>
2021-10-27 15:18:02 +0900

    delete unused class files

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 import logging
-
-from src.account import *
-from src.config import *
 from src.manager import *
 import sys
-from src.util import *
 from src.socket_server import *
-
-# COUPLE_FILE_PATH =
-
-# coin_name, balance, interval, repeat
-TEST_JSON = {'coin_name': '리플',
-             'balance': 220000,
-             'interval': 1,
-             'repeat': False}
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='[%(asctime)s] %(message)s')
@@ -25,6 +13,5 @@ if __name__ == '__main__':
     socket.setManager(manager)
     while True:
         socket.wait_for_connection()
-    # manager.do_start(None, 'infinite', TEST_JSON)
 
 
