fcdc51bc7905d2460b83b723fb236d351ed59f2b
Hanwool Lee <hanwool26@gmail.com>
2022-01-06 14:45:01 +0900

    add report feature for checking profit easily

--- src/report.py
+++ src/report.py
@@ -1,8 +1,35 @@
+import os
+import datetime
+# from src.event import PER_BUY
+PER_BUY = 40
+Today = datetime.date.today()
+Now = datetime.datetime.now()
+
 OUTPUT_DIR = '/usr/share/coin_trader/output/'
+OUTPUT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 class Report():
     def __init__(self):
+        print('Init Report()')
         file_url = OUTPUT_DIR
 
-    def release(self, name, avg_price, sold_price, invest_rate, profit, progress):
-        pass
+    def make_file_path(self) -> str:
+        y_m_str = 'report_%s-%s.txt' % (Today.year, Today.month)
+        file_path = os.path.join(OUTPUT_DIR, y_m_str)
+        return file_path
+
+    def make_report_str(self, name, sold_price, avg_price, amount, invest_rate, buy_count):
+        profit = (sold_price - avg_price) * amount
+        progress = (buy_count / PER_BUY) * 100
+        report_str = '%s / %s /평단가 : %s원 / 매도가 : %s원 / 이익율 : %s%% / 손익 : %s원  / 진행률 : %s%%' % (
+            Now, name, avg_price, sold_price, invest_rate, profit, progress)
+        return report_str
+
+    def save_report(self, name, sold_price, avg_price, amount, invest_rate, buy_count):
+        file_path = self.make_file_path()
+        report_str = self.make_report_str(name,sold_price,avg_price,amount,invest_rate,buy_count)
+        with open(file_path, "a") as f:
+            f.write(report_str)
+
+r = Report()
+r.save_report('도지', 300, 200, 100, 10, 10)
