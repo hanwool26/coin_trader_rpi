2331be7698dc90c18adf6115677fd599a36bb378
hanwool <hanwool26@gmail.com>
2022-02-03 21:10:08 +0900

    bug fixed in report which can be saved for showing time rightly

--- src/report.py
+++ src/report.py
@@ -2,17 +2,17 @@ import os
 import datetime
 import logging
 from src.event import PER_BUY
-Today = datetime.date.today()
-Now = datetime.datetime.now()
 
 OUTPUT_DIR = '/usr/share/coin_trade/output/'
 
 class Report():
     def __init__(self):
         file_url = OUTPUT_DIR
+        Today = None
+        Now = None
 
     def make_file_path(self) -> str:
-        y_m_str = 'report_%s-%s.txt' % (Today.year, Today.month)
+        y_m_str = 'report_%s-%s.txt' % (self.Today.year, self.Today.month)
         file_path = os.path.join(OUTPUT_DIR, y_m_str)
         return file_path
 
@@ -20,10 +20,12 @@ class Report():
         profit = (sold_price - avg_price) * amount
         progress = (buy_count / PER_BUY) * 100
         report_str = '%s / %s / 평단가 : %s원 / 매도가 : %s원 / 이익율 : %s%% / 손익 : %s원  / 진행률 : %s%%\n' % (
-            Now, name, avg_price, sold_price, invest_rate, profit, progress)
+            self.Now, name, avg_price, sold_price, invest_rate, profit, progress)
         return report_str
 
     def save_report(self, name, sold_price, avg_price, amount, invest_rate, buy_count):
+        self.Today = datetime.date.today()
+        self.Now = datetime.datetime.now()
         file_path = self.make_file_path()
         report_str = self.make_report_str(name,sold_price,avg_price,amount,invest_rate,buy_count)
         logging.info(report_str)
